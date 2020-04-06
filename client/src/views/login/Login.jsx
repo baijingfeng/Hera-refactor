fe58efdca7c15cff274b9b99bbebbf6755327b48
baijingfeng <2596239285@qq.com>
2020-04-06 22:23:56 +0800

    chore: 更新项目

---
+++ client/src/views/login/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <div className="login-header">
+          <img src="" alt=""/>
+          <h1>login</h1>
+        </div>
+        <div className="login-content">
+          
+        </div>
+      </div>
+    )
+  }
+}
