5705d3a8a088866389a1488ddc786fff1b1d26bf
bai.jingfeng <bai.jingfeng@qq.com>
2020-05-13 18:46:18 +0800

    add: 完善一些仓库查询-库存查询的功能

--- client/src/pages/storage-query/store/components/StoreFilter.jsx
+++ client/src/pages/storage-query/store/components/StoreFilter.jsx
@@ -1,85 +1,71 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Card, Form, Select, DatePicker, Button, Input } from 'antd'
-import { ReloadOutlined, SearchOutlined } from '@ant-design/icons'
+import { Card, Form, Select, DatePicker } from 'antd'
 
-import { getVendors } from '../../../../utils'
-import { dateRanges } from '../../../../configs'
+import {
+	useResetForm,
+	getVendors,
+	getTypeOptions,
+	generateOptions,
+} from '../../../../utils'
+import { RECORD_TYPES, dateRanges, formStyle } from '../../../../configs'
+import { SearchButton, ResetButton } from '../../../../components'
 
 const FormItem = Form.Item
-const { Option } = Select
 const { RangePicker } = DatePicker
 
-const formStyle = {
-	display: 'flex',
-	flexWrap: 'wrap',
-	justifyContent: 'space-between',
-}
-
-const getPlanOptions = plans =>
-	plans.map(plan => ({
-		value: plan._id,
-		label: plan.name,
-		pinyin: '',
-	}))
-
 export const StoreFilter = () => {
-	const projects = useSelector(store => store.system.projects)
+	const store = useSelector(state => state.system.store)
+	const { form, resetForm } = useResetForm()
 
-	const onChange = (dates, dateStrings) => {
-		console.log('From: ', dates[0], ', to: ', dates[1])
-		console.log('From: ', dateStrings[0], ', to: ', dateStrings[1])
-	}
+	const { _id, company, name, pinyin } = store || {}
+
+	const options = [
+		{
+			value: _id,
+			label: company + name,
+			pinyin: pinyin,
+		},
+	]
 
 	const onFinish = () => {
 		console.log('object')
 	}
 
-	const projectOptions = getVendors(projects).map(project => (
-		<Option key={project._id} value={project._id}>
-			{project.company + project.name}
-		</Option>
-	))
-
+	console.log('options', options)
 	return (
-		<Form hideRequiredMark onFinish={onFinish}>
-			<Card
-				title={
-					<Button type="dashed" htmlType="reset">
-						<ReloadOutlined />
-						重置
-					</Button>
-				}
-				extra={
-					<Button type="primary" htmlType="submit">
-						<SearchOutlined />
-						查询
-					</Button>
-				}
+		<Card
+			title={<ResetButton onClick={resetForm} />}
+			extra={<SearchButton form="storeForm" />}
+		>
+			<Form
+				id="storeForm"
+				form={form}
+				style={formStyle}
+				hideRequiredMark
+				onFinish={onFinish}
 			>
-				<div style={formStyle}>
-					<FormItem name="rangeDate">
-						<RangePicker ranges={dateRanges} onChange={onChange} style={{ width: 300 }}/>
-					</FormItem>
-					<FormItem
-						name="type"
-						rules={[{ required: true, message: '请选择类型!' }]}
-					>
-						<Select style={{ width: 200 }} placeholder="类型">
-							{projectOptions}
-						</Select>
-					</FormItem>
+				<FormItem name="rangeDate">
+					<RangePicker ranges={dateRanges} style={{ width: 300 }} />
+				</FormItem>
+				<FormItem
+					name="type"
+					rules={[{ required: true, message: '请选择类型!' }]}
+				>
+					<Select style={{ width: 200 }} placeholder="类型">
+						{getTypeOptions(RECORD_TYPES).map(generateOptions)}
+					</Select>
+				</FormItem>
 
-					<FormItem
-						name="project"
-						rules={[{ required: true, message: '请选择仓库!' }]}
-					>
-						<Select style={{ width: 400 }} placeholder="选择要查询的仓库">
-							{projectOptions}
-						</Select>
-					</FormItem>
-				</div>
-			</Card>
-		</Form>
+				<FormItem
+					name="project"
+					rules={[{ required: true, message: '请选择仓库!' }]}
+				>
+					<Select style={{ width: 400 }} placeholder="选择要查询的仓库">
+						{options.map(generateOptions)}
+					</Select>
+				</FormItem>
+			</Form>
+		</Card>
 	)
 }
--- client/src/utils/getOptions/generateOptions.jsx
+++ client/src/utils/getOptions/generateOptions.jsx
@@ -5,7 +5,7 @@ const { Option } = Select
 /**
  * 生成形如value-label类型的选择项
  */
-export const generateOptions = ({ value, label }) => (
+export const generateOptions = ({ value = '', label }) => (
 	<Option key={value} value={value}>
 		{label}
 	</Option>
